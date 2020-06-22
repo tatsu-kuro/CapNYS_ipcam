e0e27533525955127e7878d275eaca7a8f260f4e
tatsu-kuro <tatsuakikuroda@me.com>
2020-06-22 22:27:56 +0900

    またコミットを要求するか？

--- CapNYS2/CapNYS2.h
+++ CapNYS2/CapNYS2.h
@@ -11,7 +11,12 @@
 #include "resource.h"		// メイン シンボル
 #define Vertxt "CapNYS2(IPcamera) Version4.5 - 2019/12/25"
 #define English false
-//グルグル回らないようだ
+//git init
+//.gitignoreと.gitattribueをコピー
+//git add .
+//git remote add origin https ://github.com/tatsu-kuro/CapNYS_ipcam.git
+//Visual Studioを終了して、再起動するとgit、githubが有効になっている。
+//もしかすると再起動をgit initの直後に行うと、.gitignoreなどのコピーは必要ないのかもしれない。
 // CCapNYS2App:
 // このクラスの実装については、CapNYS2.cpp を参照してください。
 //
